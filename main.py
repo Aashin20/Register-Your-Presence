01c0d084f5627977fc4dd9a517d01067d0a95bdf
Aashin C Anil <148208390+Aashin20@users.noreply.github.com>
2025-04-27 19:28:36 +0530

    Create event endpoint

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
-from utils.db import init_postgres, init_mongo
-
+from utils.db import Database
+from pydantic import BaseModel, Field
+from typing import List, Optional
+from datetime import date, time
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
-from utils.db import Database, PostgresDB
+from utils.db import Database #PostgresDB
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     Database.initialize()
-    PostgresDB.initialize()
+    #PostgresDB.initialize()
     yield
     Database.close()
-    PostgresDB.close()
+    #PostgresDB.close()
 
 app = FastAPI(lifespan=lifespan)
 
@@ -25,12 +27,48 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+class GeoLocation(BaseModel):
+    lat: float  
+    lng: float 
+
+class Event(BaseModel):
+    event_name: str
+    event_desc: str
+    event_location: GeoLocation
+    event_sdate: date
+    event_stime: time
+    event_edate: date
+    event_etime: time
+    attendees: Optional[List[str]] = None
+
 @app.post("/login")
 async def login(email: str, password: str, request: Request):
     if email == "admin@gmail.com" and password == "admin":
         return {"message": "Login successful"}
     else:
         return {"message": "Invalid credentials"}
+    
+
+@app.post("/create_event")
+async def create_event(info: Event):
+    db = Database.get_db().EventDetails
+    exists = db.find_one({"event_name": info.event_name})
+    if exists:
+        return {"status": "Event already exists"}
+    event_dict = info.model_dump()
+    for field in ['event_sdate', 'event_edate']:
+        event_dict[field] = event_dict[field].isoformat()
+    for field in ['event_stime', 'event_etime']:
+        event_dict[field] = event_dict[field].isoformat()
+    event_dict['event_location'] = {
+        "type": "Point",
+        "coordinates": [event_dict['event_location']['lng'], event_dict['event_location']['lat']]
+    }
+    db.insert_one(event_dict)
+    return {"status": "Event created successfully"}
+
+
+
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8001, log_level="debug")
